5626edd33c3a6397f779737b5e862c0d51b5a155
FurkanMusa <furkan.m.titrek@gmail.com>
2024-09-10 16:55:02 +0300

    Animation Start :P

--- 04 Animation/src/main.ts
+++ 04 Animation/src/main.ts
@@ -1,12 +1,10 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import Stats from 'three/addons/libs/stats.module.js'
 
 // Scene
 const scene = new THREE.Scene()
-
-// Grid
-scene.add(new THREE.GridHelper())
 
 // Skybox
 scene.background = new THREE.CubeTextureLoader()
@@ -15,33 +13,42 @@ scene.background = new THREE.CubeTextureLoader()
 scene.backgroundBlurriness = 0.1
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 1.5
-camera.position.y = 0.5
-
-// Canvas
-const canvas = document.getElementById('canvasim') as HTMLCanvasElement
 
 // Renderer
-const renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true})
-renderer.setSize(400, 400)
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+})
 
 // Cube
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
 
 const cube = new THREE.Mesh(geometry, material)
-cube.position.y = 0.5
 scene.add(cube)
 
 // Orbit Controls
 new OrbitControls(camera, renderer.domElement)
 
+// Stats
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+
 // ~ A N I M A T E ~ //
 function animate() {
   requestAnimationFrame(animate)
 
-  camera.lookAt(0, 0.5, 0)
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
+
+  stats.update()
   
   renderer.render(scene, camera)
 }
