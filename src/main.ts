b4a295e57e7e62ae743d157d6c4be65fcd8f0808
furkan.titrek <furkan.titrek@tubitak.gov.tr>
2024-09-10 15:30:52 +0300

    add multiple scenes

--- src/main.ts
+++ src/main.ts
@@ -4,28 +4,44 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import Stats from 'three/addons/libs/stats.module.js'
 import { GUI } from 'dat.gui'
 
+// Scene I
+const sceneA = new THREE.Scene()
 
-const scene = new THREE.Scene()
+// Scene II
+const sceneB = new THREE.Scene()
+sceneA.background = new THREE.Color(0x151517)
 
+// Scene III
+const sceneC = new THREE.Scene()
+
+// Skybox
+sceneC.background = new THREE.CubeTextureLoader()
+.setPath('https://sbcode.net/img/')
+.load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'])
+sceneC.backgroundBlurriness = 0
+
+// Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 1.5
 
+// Renderer
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
+// Window Properties
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
   renderer.setSize(window.innerWidth, window.innerHeight)
 })
 
+// Cube
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
 
 const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-
+sceneA.add(cube)
 
 // Orbit Controls
 new OrbitControls(camera, renderer.domElement)
@@ -36,6 +52,7 @@ document.body.appendChild(stats.dom)
 
 // GUI
 const gui = new GUI()
+gui.close()
 
 const guiCube = gui.addFolder("Cube")
 guiCube.add(cube.rotation, "x", 0, Math.PI * 2)
@@ -44,8 +61,24 @@ guiCube.add(cube.rotation, "z", 0, Math.PI * 2)
 
 const guiCamera = gui.addFolder("Camera")
 guiCamera.add(camera.position, "z", 0, 20)
-guiCamera.open()
 
+let activeScene = sceneA
+const setScene = {
+  sceneA: () => {
+    activeScene = sceneA
+  },
+  sceneB: () => {
+    activeScene = sceneB
+  },
+  sceneC: () => {
+    activeScene = sceneC
+  },
+}
+
+const guiScene = gui.addFolder("Scenes")
+guiScene.add(setScene, 'sceneA').name('Scene A')
+guiScene.add(setScene, 'sceneB').name('Scene B')
+guiScene.add(setScene, 'sceneC').name('Scene C')
 
 // ~ A N I M A T E ~ //
 function animate() {
@@ -57,7 +90,7 @@ function animate() {
   stats.update() // Can be used with begin(), end().
   gui.updateDisplay()
   
-  renderer.render(scene, camera)
+  renderer.render(activeScene, camera)
 }
 
 
